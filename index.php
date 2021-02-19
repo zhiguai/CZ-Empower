2d8c002de01c5e20b23109c8556a948f8421c75e
zhiguai <2635435377@qq.com>
2021-02-20 01:27:12 +0800

    master

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-/*
- * @Description: 
- * @Autor: 吃纸怪
- * @QQ: 2903074366
- * @Email: 2903074366@qq.com
- * @QQgroup: 801235342
- * @Github: https://github.com/zhiguai
- */
-echo "暂不开放1";
+echo "暂不开放";
 
 ?>
