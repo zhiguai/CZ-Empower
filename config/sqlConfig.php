2d279ad4798aa5efe333ce3fd5a7960561653e49
zhiguai <2635435377@qq.com>
2021-02-20 15:17:48 +0800

    master

--- config/sqlConfig.php
+++ config/sqlConfig.php
@@ -1,8 +1,8 @@
 <?php
 //数据库信息
 define('DB_HOST','localhost');//Mysql数据库服务器
-define('DB_USER','2b_com');//Mysql数据库用户名
+define('DB_USER','123456zxc');//Mysql数据库用户名
 define('DB_PASSWORD','123456zxc');//Mysql数据库密码
-define('DB_DATABASE','123456zxc');//Mysql数据库名
+define('DB_DATABASE','2b_com');//Mysql数据库名
 define('DB_PORT','3306');//Mysql数据库名
 ?>
