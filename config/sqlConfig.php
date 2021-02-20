666f6fd1612d5d6e6f23fd58dacca72912d0434d
zhiguai <2635435377@qq.com>
2021-02-20 15:15:31 +0800

    master

--- config/sqlConfig.php
+++ config/sqlConfig.php
@@ -1,9 +1,8 @@
-<?php
-
+<?php/
 //数据库信息
 define('DB_HOST','localhost');//Mysql数据库服务器
-define('DB_USER','3i4ezq629i222');//Mysql数据库用户名
-define('DB_PASSWORD','sLSVvi83B0');//Mysql数据库密码
-define('DB_DATABASE','3i4ezq629i222');//Mysql数据库名
+define('DB_USER','2b_com');//Mysql数据库用户名
+define('DB_PASSWORD','123456zxc');//Mysql数据库密码
+define('DB_DATABASE','123456zxc');//Mysql数据库名
 define('DB_PORT','3306');//Mysql数据库名
 ?>
